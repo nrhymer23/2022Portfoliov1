f9af6ac3735a4e1442541da69f7b66285385cf79
nrhymer23 <38295535+nrhymer23@users.noreply.github.com>
2022-01-11 17:37:15 -0500

    Background & Hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        The puropse.. Showcase of projects and skills
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;

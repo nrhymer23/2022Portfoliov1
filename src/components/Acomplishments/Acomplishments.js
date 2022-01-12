d204a87d8ab4d5298f85da3a9fbc83958e8fd20b
nrhymer23 <38295535+nrhymer23@users.noreply.github.com>
2022-01-12 12:48:31 -0500

    Accomplishment Section setup

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
+import { CarouselButtonDot } from '../TimeLine/TimeLineStyles';
 import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
 
 const data = [
@@ -11,9 +12,17 @@ const data = [
 ];
 
 const Acomplishments = () => (
-  <div>
-    Acomplishments
-  </div>
+ <Section>
+   <SectionTitle>Personal Accomplishments</SectionTitle>
+   <Boxes>
+     {data.map((card,index) => (
+       <Box key={index}>
+         <BoxNum>{card.number}+</BoxNum>
+         <BoxText>{card.text}</BoxText>
+       </Box>
+     ))}
+   </Boxes>
+ </Section>
 );
 
 export default Acomplishments;
